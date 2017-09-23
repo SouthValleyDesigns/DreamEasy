70ba4f267a090e48cf4a7d766ff0408734422c02
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-09-23 01:29:40 -0700

    Fix urls merge conflict

--- DreamEasyApp/urls.py
+++ DreamEasyApp/urls.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from django.conf.urls import url
-import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-=======
 from django.conf.urls import url, include
 from DreamEasyApp import views
 
@@ -12,5 +5,4 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^art/', views.art),
     url(r'^members/', views.members),
->>>>>>> d54b61c1e378d59f102731a75507dc20f1c551b4
 ]

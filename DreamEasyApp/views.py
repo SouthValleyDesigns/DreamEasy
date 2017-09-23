8eaaa79701cb07f9f6f7c4858dfddd48b23fd790
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2017-09-23 01:25:40 -0700

    Slight urls update

--- DreamEasyApp/views.py
+++ DreamEasyApp/views.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    pass
+
+def art(request):
+    pass
+
+def members(request):
+    pass

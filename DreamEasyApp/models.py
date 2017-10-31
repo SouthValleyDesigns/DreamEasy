40a86086f320b53067c355ba7c51835776b9c8c1
MattyJ <Matthewsages@gmail.com>
2017-10-31 14:37:20 -0700

    Membertab (#7)
    * Update footer color
    
    * Add member model
    
    * Add render of members to template
    
    * Update style
    
    * Remove sass cache

--- DreamEasyApp/models.py
+++ DreamEasyApp/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.name
--- DreamEasyApp/views.py
+++ DreamEasyApp/views.py
@@ -2,15 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Member
 
 
 def index(request):
     return render(request, 'index.html')
 
 def members(request):
-    return render(request, 'members.html')
+    member_list = Member.objects.order_by('name')
+
+    return render(request, 'members.html', {'member_list':member_list})
 
 def art(request):
     return render(request, 'art.html')

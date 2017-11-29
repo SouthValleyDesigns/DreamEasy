c0443719fe0206f6d0508acc82dc4e93c31c29a7
MattyJ <Matthewsages@gmail.com>
2017-11-28 17:46:29 -0800

    Heroku (#9)
    * Ad Procfile
    
    * Add whitenoise
    
    * Fix staticfile storage
    
    * Fix name
    
    * Update allowed hosts
    
    * Cleanup

--- DreamEasy/settings.py
+++ DreamEasy/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    '127.0.0.1'
+    '127.0.0.1',
+    'dream-easy.herokuapp.com',
 ]
 
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn DreamEasy.wsgi --log-file -

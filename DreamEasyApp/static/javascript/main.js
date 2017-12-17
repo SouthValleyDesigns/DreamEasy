39313ac4991f7fcdbef63d764fdb27265a525e0c
verycumbersome <matthewsages@gmail.com>
2017-12-16 21:58:42 -0800

    Add nanobar

--- DreamEasyApp/static/javascript/main.js
+++ DreamEasyApp/static/javascript/main.js
@@ -1,14 +1,16 @@
-var options = {
-	classname: 'my-class',
-  id: 'my-id',
-	target: document.getElementById('myDivId')
-};
+$(document).ready(function() {
+    var options = {
+    	classname: 'paper',
+        // id: 'my-id',
+    	// target: document.getElementById('myDivId')
+    };
 
-var nanobar = new Nanobar( options );
+    var nanobar = new Nanobar( options );
+    // var Nanobar = require('/DreamEasy/bower_components/nanobar');
+    //move bar
+    nanobar.go( 30 ); // size bar 30%
+    nanobar.go( 76 ); // size bar 76%
 
-//move bar
-nanobar.go( 30 ); // size bar 30%
-nanobar.go( 76 ); // size bar 76%
-
-// size bar 100% and and finish
-nanobar.go(100);
+    // size bar 100% and and finish
+    nanobar.go(100);
+});

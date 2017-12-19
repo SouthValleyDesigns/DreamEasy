b4ef76c09cc1cad79597dfb1f91a41c699d9a82f
verycumbersome <matthewsages@gmail.com>
2017-12-18 21:37:00 -0800

    Update CDNs for chrom eand andriod

--- DreamEasyApp/static/javascript/main.js
+++ DreamEasyApp/static/javascript/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     };
 
     var nanobar = new Nanobar( options );
-    
+
     //move bar
     nanobar.go( 30 ); // size bar 30%
     nanobar.go( 76 ); // size bar 76%

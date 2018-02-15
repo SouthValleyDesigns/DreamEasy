434c10c57e1a235c16ed8197eec71701e62a78f7
MattyJ <Matthewsages@gmail.com>
2018-02-14 21:12:02 -0800

    Ssl (#13)
    * SSL update
    
    * Change backgrounds
    
    * Update artists
    
    * Cleanup
    
    * Update photos and names
    
    * Remove ssl
    
    * Restructure front page

--- DreamEasyApp/admin.py
+++ DreamEasyApp/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Member, Admin
+from .models import Member, Admin, SoundcloudPost
 
 admin.site.register(Member)
 admin.site.register(Admin)
+admin.site.register(SoundcloudPost)
--- DreamEasyApp/models.py
+++ DreamEasyApp/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from datetime import datetime   
 
 class Admin(models.Model):
     name = models.CharField(max_length=200)
@@ -17,6 +18,14 @@ class Member(models.Model):
     description = models.CharField(max_length=280, default='Add description of artists here')
     photo_url = models.CharField(max_length=200, default='http://www.stallerdental.com/wp-content/uploads/2016/12/user-icon.png')
     soundcloud_url = models.CharField(max_length=100, default='#')
+
+    def __str__(self):
+        return self.name
+
+class SoundcloudPost(models.Model):
+    name = models.CharField(max_length=200, default='Add the name of the release here')
+    embed_link = models.CharField(max_length=2000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.name
